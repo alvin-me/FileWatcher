962cc49da0d352ef69625ef45d03d4e5731058b1
unknown <19014514@shal2483.corp.carestreamhealth.com>
2015-09-25 11:53:07 +0800

    add status bar for display of additonal informations.

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -13,6 +13,8 @@ namespace FileWatcher
 
         private bool _watching;
         private string[] _watchingFilter;
+        private string _message;
+        private string _messageBackup;
         /*The Variables are meant to be readonly as we mustnot change the address of any of them by creating new instances.
          *Problem with new istances is that since address changes the binding becomes invalid.
          *Instantiate all the variables in the constructor.
@@ -22,7 +24,8 @@ namespace FileWatcher
         private readonly FileSystemWatcher _fileSystemWatcher;
         private readonly ICommand _toggleWatchingCmd;
         private readonly ICommand _openSettingWindowCmd;
-        
+        private readonly ICommand _clearCollectionCmd;
+        private readonly ICommand _saveCollectionCmd;
 
         #endregion
 
@@ -34,8 +37,11 @@ namespace FileWatcher
             _informations = new ObservableCollection<FileInformation>();
             _toggleWatchingCmd = new RelayCommand(ToggleWatching, CanToggleWatching);
             _openSettingWindowCmd = new RelayCommand(OpenSettingWindow, CanOpenSettingWindow);
+            _clearCollectionCmd = new RelayCommand(ClearCollection, CanClearCollection);
+            _saveCollectionCmd = new RelayCommand(SaveCollection, CanSaveCollection);
             _fileSystemWatcher = createWatcher();
             updateWatcherFromConfigFile();
+            LogInfo("未开始监听");
         }
 
         #endregion
@@ -151,6 +157,23 @@ namespace FileWatcher
             }
         }
 
+        /// <summary>
+        /// Gets or Sets Log Message. Ready to be binded to UI.
+        /// Impelments INotifyPropertyChanged which enables the binded element to refresh itself whenever the value changes.
+        /// </summary>
+        public string LogMsg
+        {
+            get { return _message; }
+            set
+            {
+                if (_message != value)
+                {
+                    _message = value;
+                    OnPropertyChanged("LogMsg");
+                }
+            }
+        }
+
         #endregion
 
         #region Commands
@@ -164,6 +187,16 @@ namespace FileWatcher
         /// Gets the OpenSettingWindowCmd. Used for Open setting window Operations
         /// </summary>
         public ICommand OpenSettingWindowCmd { get { return _openSettingWindowCmd; } }
+
+        /// <summary>
+        /// Gets the ClearCollectionCmd. Used for Open setting window Operations
+        /// </summary>
+        public ICommand ClearCollectionCmd { get { return _clearCollectionCmd; } }
+
+        /// <summary>
+        /// Gets the SaveCollectionCmd. Used for Open setting window Operations
+        /// </summary>
+        public ICommand SaveCollectionCmd { get { return _saveCollectionCmd; } }
 
         /// <summary>
         /// CanToggleWatching operation of the ToggleWatching.
@@ -186,9 +219,15 @@ namespace FileWatcher
         {
             Watching = !Watching;
             if (Watching)
+            {
                 _fileSystemWatcher.EnableRaisingEvents = true;
+                LogInfo("正在监听...");
+            }
             else
+            {
                 _fileSystemWatcher.EnableRaisingEvents = false;
+                LogInfo("未开始监听");
+            }
         }
 
         /// <summary>
@@ -215,10 +254,81 @@ namespace FileWatcher
                 updateWatcherFromConfigFile();
         }
 
+        /// <summary>
+        /// CanClearCollection operation of the ClearCollection.
+        /// Tells us if the control is to be enabled or disabled.
+        /// This method will be fired repeatedly as long as the view is open.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool CanClearCollection(object obj)
+        {
+            return FileInformations.Count > 0;
+        }
+
+        /// <summary>
+        /// Add operation of the ClearCollectionCmd.
+        /// Operation that will be performormed on the control click.
+        /// </summary>
+        /// <param name="obj"></param>
+        public void ClearCollection(object obj)
+        {
+            FileInformations.Clear();
+            ResetFileInformation();
+        }
+
+        /// <summary>
+        /// CanSaveCollection operation of the SaveCollection.
+        /// Tells us if the control is to be enabled or disabled.
+        /// This method will be fired repeatedly as long as the view is open.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public bool CanSaveCollection(object obj)
+        {
+            return FileInformations.Count > 0;
+        }
+
+        /// <summary>
+        /// Add operation of the SaveCollectionCmd.
+        /// Operation that will be performormed on the control click.
+        /// </summary>
+        /// <param name="obj"></param>
+        public void SaveCollection(object obj)
+        {
+            StreamWriter sw = null;
+            try
+            {
+                string fileName = Properties.Settings.Default.LogFolder + "\\" + DateTime.Now.ToString("yyyyMMdd hh-mm-ss") + ".log";
+                sw = new StreamWriter(fileName);
+                foreach (var item in FileInformations)
+                {
+                    string str = "[" + item.Id + "][" + item.EventType + "][" + item.FileName + "][" + item.FilePath + "]";
+                    sw.WriteLine(str);
+                }
+                LogInfo("日志已保存到" + fileName, 5000);
+            }
+            catch (Exception e)
+            {
+                LogInfo(e.Message + "," + e.StackTrace);
+            }
+            finally
+            {
+                if (sw != null)
+                {
+                    sw.Close();
+                }
+            }
+        }
+
         #endregion
 
         #region Private Methods
 
+        /// <summary>
+        /// Create FileSystemWatcher
+        /// </summary>
+        /// <returns></returns>
         private FileSystemWatcher createWatcher()
         {
             FileSystemWatcher watcher = new FileSystemWatcher();
@@ -230,6 +340,9 @@ namespace FileWatcher
             return watcher;
         }
 
+        /// <summary>
+        /// Update FileSystemWatcher from Config File
+        /// </summary>
         private void updateWatcherFromConfigFile()
         {
             this._fileSystemWatcher.IncludeSubdirectories = Properties.Settings.Default.WatchSubDir;
@@ -242,6 +355,9 @@ namespace FileWatcher
             }
         }
 
+        /// <summary>
+        /// FileSystemEventHandler Delegate
+        /// </summary>
         private void fileSystemWatching(object sender, FileSystemEventArgs e)
         {
             Application.Current.Dispatcher.Invoke((Action)(() =>
@@ -259,6 +375,39 @@ namespace FileWatcher
                     FilePath = e.FullPath };
                 FileInformations.Add(information);
             }));
+        }
+
+        /// <summary>
+        /// Resets the FileInformation properties which will in turn auto reset the UI aswell
+        /// </summary>
+        private void ResetFileInformation()
+        {
+            Id = 0;
+            TimeStamp = string.Empty;
+            EventType = string.Empty;
+            FileName = string.Empty;
+            FilePath = string.Empty;
+        }
+
+        /// <summary>
+        /// Set LogMsg properties which will in turn auto reset statusbar
+        /// </summary>
+        /// <param name="msg">message</param>
+        /// <param name="delay">delay time of message rollback</param>
+        private void LogInfo(string msg, int delay = 0)
+        {
+            _messageBackup = LogMsg;
+            LogMsg = msg;
+
+            // set delay timer to roll back message.
+            if (delay > 0)
+            {
+                System.Threading.Tasks.Task.Factory.StartNew(() =>
+                {
+                    System.Threading.Thread.Sleep(delay);
+                    LogInfo(_messageBackup);
+                });
+            }
         }
 
         #endregion
